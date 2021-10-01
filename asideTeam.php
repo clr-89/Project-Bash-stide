6a2025892580068bd0e3e4bf1ca62099f8515862
clr-89 <clr.89@gmail.com>
2021-10-01 14:33:38 +0200

    modif prev next

--- asideTeam.php
+++ asideTeam.php
@@ -90,7 +90,7 @@
         </div>
 
     <div class='container-prev-next'></div>
-  <!-- Next and previous buttons --
+  <!-- Next and previous buttons -->
   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
   <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
