ff4ca754582255d7d4f30db1a8ffa52c28b2f7a2
JonathanPoitrenaud <poitrenaudjonathan@gmail.com>
2021-10-06 15:38:38 +0200

    modification on index.php

--- index.php
+++ index.php
@@ -12,56 +12,51 @@
     <title>Bash-Stide</title>
 </head>
 <body>
-<?php include 'navbar.php'; ?>
-<div id="container">
-    <?php require 'activity.php'; ?>
-    <main class="mainContainer">
-
-        <section class="presentation">
-
-            <article class="size">
-                <img src="images/Putin_place-de-stalingrad.jpg" alt="lion-stalin">
-            </article>
-            <article class="mainText">
-                <h1>PRÉSENTATION</h1>
-                <p>
-                    BashStide, est une start-up rugissante implantée à Bordeaux depuis 2021.
-                    La qualité de nos prestations unique en Europe vous feront vivre des instants inoubliables.
-                </p>
-            </article>
-        </section>
-
-      <div>
-          <h2 id="activity" >ACTIVITÉS</h2>
-          <?php foreach ($activities as $activityType => $numberActivities) : ?>
-              <?php foreach ($numberActivities as $titleActivities => $detailActivity) : ?>
-                  <section class="<?= $activityType %2 === 0?"row":"reverse" ?>">
-                      <article class="mainImg">
-                          <img src= "<?= $detailActivity['img']?>" alt="lion-eat"/>
-                      </article>
-                      <article class="mainText">
-                          <h3><?= $titleActivities?></h3>
-                            <p>
-                                <?= $detailActivity['detail']?><br>
-                                Le prix est de <?= $detailActivity['prix']?> €
-                            </p>
-                        <div class="button">
-                            <button><a href="contact.php">Reservation</a></button>
-                        </div>
-            </article>
-            </section>
-              <?php endforeach ; ?>
-          <?php endforeach ; ?>
-      </div>
-
-        <?php require 'asideTeam.php' ?>
-
-    </main>
-    <footer>
-        <?php include 'footer.php'; ?>
-    </footer>
-</div>
-<script type="text/javascript" src="script.js"></script>
+    <?php include 'navbar.php'; ?>
+        <div id="container">
+            <?php require 'activity.php'; ?>
+                <main class="mainContainer">
+                    <section class="presentation">
+                        <article class="size">
+                            <img src="images/Putin_place-de-stalingrad.jpg" alt="lion-stalin">
+                        </article>
+                        <article class="mainText">
+                            <h1>PRÉSENTATION</h1>
+                                <p>
+                                    BashStide, est une start-up rugissante implantée à Bordeaux depuis 2021.
+                                    La qualité de nos prestations unique en Europe vous feront vivre des instants inoubliables.
+                                </p>
+                        </article>
+                    </section>
+                    <div>
+                        <h2 id="activity" >ACTIVITÉS</h2>
+                            <?php foreach ($activities as $activityType => $numberActivities) : ?>
+                                <?php foreach ($numberActivities as $titleActivities => $detailActivity) : ?>
+                                    <section class="<?= $activityType %2 === 0?"row":"reverse" ?>">
+                                        <article class="mainImg">
+                                            <img src= "<?= $detailActivity['img']?>" alt="lion-eat"/>
+                                        </article>
+                                        <article class="mainText">
+                                            <h3><?= $titleActivities?></h3>
+                                                <p>
+                                                    <?= $detailActivity['detail']?><br>
+                                                    Le prix est de <?= $detailActivity['prix']?> €
+                                                </p>
+                                            <div class="button">
+                                                <button><a href="contact.php">Reservation</a></button>
+                                            </div>
+                                        </article>
+                                    </section>
+                                  <?php endforeach ; ?>
+                              <?php endforeach ; ?>
+                      </div>
+                    <?php require 'asideTeam.php' ?>
+                </main>
+                <footer>
+                    <?php include 'footer.php'; ?>
+                </footer>
+        </div>
+    <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
 
